a492ee7e76fc88acb9b1f4dcf958571fcd03d87b
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2026-03-07 00:03:12 +0100

    Drop redundant Refine instance
    Implied by the `Inj` instance

--- rocq-brick-libstdcpp/proof/iostream/pred.v
+++ rocq-brick-libstdcpp/proof/iostream/pred.v
@@ -16,12 +16,6 @@ Require Import skylabs.brick.libstdcpp.iostream.inc_iostream_cpp.
 (** TODO upstream *)
 #[global] Bind Scope bs_scope with cstring.t.
 (* We only have `Bind Scope bs_scope with t.` inside `Module cstring.` *)
-
-(** TODO upstream to auto *)
-#[global] Instance refine_bs_app' (str a b : BS.t) :
-  Refine1 true true (str ++ a = str ++ b)%bs [a = b].
-Proof. tac_refine. exact: (inj (BS.append str)). Qed.
-
 (** TODO upstream END *)
 
 Section with_cpp.
