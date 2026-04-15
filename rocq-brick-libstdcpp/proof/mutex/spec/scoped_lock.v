4e6f40930843e9a70df0de8d335e65abe987c950
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2026-04-15 19:42:55 +0200

    scoped_lock: add learning hint

--- rocq-brick-libstdcpp/proof/mutex/spec/scoped_lock.v
+++ rocq-brick-libstdcpp/proof/mutex/spec/scoped_lock.v
@@ -19,6 +19,8 @@ Module scoped_lock.
       Context {σ : genv}.
       Context `{HAS_THREADS : !HasStdThreads Σ}.
 
+      #[global] Instance: LearnEqF1 R := ltac:(solve_learnable).
+
       cpp.spec
       "std::scoped_lock<...<std::mutex, std::mutex>>::scoped_lock(std::mutex&, std::mutex&)"
       (* "std::scoped_lock<std::mutex, std::mutex>::scoped_lock(std::mutex&, std::mutex&)" *)
@@ -67,79 +69,6 @@ Module scoped_lock.
         verify_spec; go.
         iExists emp; go.
         iExists emp; go.
-        Fail solve [ego].
-        ework.
-        ego.
-
-        (**
-        (** Weird failure! *)
-        ego.
-  match args_for <$> as_function (normalize_type ?ty0) with
-  | Some _ =>
-  eval evaluation_order.nd (map (wp_operand ?tu ?ρ) ?es0)
-  (λ (vs : list val) (free : FreeTemps),
-  builtins.wp_builtin ?f0 ?ty0 vs
-  (λ v : val,
-  match v with
-  | Vptr obj_ptr =>
-  if bool_decide (obj_ptr = nullptr)
-  then wp_delete_null ?default_delete ?destroyed_type (?Q Vvoid free)
-  else
-  new_delete.wp_delete_dispatch.body ?default_delete ?destroyed_type obj_ptr
-  (?Q Vvoid free)
-  | _ => False
-  end))
-  | None => errors.Errors.ERROR.body "builtin does not have function type"%bs
-  end ∗
-  match args_for <$> as_function (normalize_type ?ty) with
-  | Some _ =>
-  eval evaluation_order.nd (map (wp_operand ?tu0 ?ρ0) ?es)
-  (λ (vs : list val) (free : FreeTemps),
-  builtins.wp_builtin ?f ?ty vs
-  (λ v : val,
-  match v with
-  | Vptr obj_ptr =>
-  if bool_decide (obj_ptr = nullptr)
-  then wp_delete_null ?default_delete0 ?destroyed_type0 (?Q0 Vvoid free)
-  else
-  new_delete.wp_delete_dispatch.body ?default_delete0 ?destroyed_type0 obj_ptr
-  (?Q0 Vvoid free)
-  | _ => False
-  end))
-  | None => errors.Errors.ERROR.body "builtin does not have function type"%bs
-  end ∗
-  lock_addr
-  |-> R 1$m
-  [(?p, ?g, ?q,
-  ::wpOperand
-  ?ρ
-  (Edelete false ?default_delete
-  (Ecall (Ecast (Cbuiltin2fun (Tptr [...])) (Eglobal (Nglobal [...]) ?ty0)) ?es0)
-  ?destroyed_type));
-  (?p0, ?g0, ?q0,
-  ::wpOperand
-  ?ρ0
-  (Edelete false ?default_delete0
-  (Ecall (Ecast (Cbuiltin2fun (Tptr [...])) (Eglobal (Nglobal [...]) ?ty)) ?es)
-  ?destroyed_type0))] ∗ mutex.locked ?g thr ?q ∗ mutex.locked ?g0 thr ?q0 ∗
-  (?p
-  |-> mutex.R ?g ?q$m
-  (::wpOperand
-  ?ρ
-  (Edelete false ?default_delete
-  (Ecall (Ecast (Cbuiltin2fun ([...])) (Eglobal ([...]) ?ty0)) ?es0)
-  ?destroyed_type)) ∗ mutex.token ?g ?q ∗
-  ?p0
-  |-> mutex.R ?g0 ?q0$m
-  (::wpOperand
-  ?ρ0
-  (Edelete false ?default_delete0
-  (Ecall (Ecast (Cbuiltin2fun ([...])) (Eglobal ([...]) ?ty)) ?es)
-  ?destroyed_type0)) ∗ mutex.token ?g0 ?q0 -∗
-  interp source ((1 >*> FreeTemps.delete "std::mutex" m2_addr) >*>
-  FreeTemps.delete "std::mutex" m1_addr)
-  (∀ p : ptr, p |-> primR "void" 1$m Vvoid -∗ ▷ _PostPred_ p))
-        *)
       Qed.
     End with_threads.
   End with_cpp.
