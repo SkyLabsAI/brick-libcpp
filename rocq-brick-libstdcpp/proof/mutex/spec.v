f36cd996393b2bc53576dd1b670cf3fbcd8cb031
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2026-04-30 18:15:56 +0200

    mutex spec: export other specs

--- rocq-brick-libstdcpp/proof/mutex/spec.v
+++ rocq-brick-libstdcpp/proof/mutex/spec.v
@@ -1,3 +1,4 @@
 Require Export skylabs.brick.libstdcpp.mutex.spec.mutex.
 Require Export skylabs.brick.libstdcpp.mutex.spec.lock_guard.
 Require Export skylabs.brick.libstdcpp.mutex.spec.recursive_mutex.
+Require Export skylabs.brick.libstdcpp.mutex.spec.scoped_lock.
