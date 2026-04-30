67cb7d46762961c1d6abdb9d7697e68b6bebd3bb
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2026-04-30 20:20:05 +0200

    Revise mutex tests

--- rocq-brick-libstdcpp/test/mutex/test_cpp_proof.v
+++ rocq-brick-libstdcpp/test/mutex/test_cpp_proof.v
@@ -3,23 +3,25 @@ Require Import skylabs.brick.libstdcpp.mutex.spec.
 Require Import skylabs.brick.libstdcpp.test.mutex.test_cpp.
 
 Section with_cpp.
-  Context `{Σ : cpp_logic} {HAS_THREADS : HasStdThreads Σ}.
-  Context `{MOD : test_cpp.source ⊧ σ}. (* σ is the whole program *)
+  Context `{Σ : cpp_logic, σ : genv} {HAS_THREADS : HasStdThreads Σ}.
 
-  cpp.spec "test_mutex()" as test_mutex_spec with
-      (\post emp).
+  cpp.spec "test_mutex()" as test_mutex_spec from source with (\post emp).
 
   Theorem test_mutex_ok : verify[source] "test_mutex()".
-  Proof using HAS_THREADS.
+  Proof.
     verify_spec; go.
     iExists emp; go.
   Qed.
 
-  cpp.spec "test_scoped_lock()" as test_scoped_lock_spec from source with
-  (
-    \persist{thr} current_thread thr
-    \post emp
-  ).
+  cpp.spec "test_lock_guard()" as test_lock_guard_spec from source with (\post emp).
+
+  Lemma test_lock_guard_ok : verify[source] "test_lock_guard()".
+  Proof.
+    verify_spec; go.
+    iExists emp; go.
+  Qed.
+
+  cpp.spec "test_scoped_lock()" as test_scoped_lock_spec from source with (\post emp).
 
   Lemma test_scoped_lock_ok : verify[source] "test_scoped_lock()".
   Proof.
