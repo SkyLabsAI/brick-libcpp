becb34ca98417adf559b10236bdfdc9bab83ce05
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2026-05-31 00:01:50 +0200

    Alternative fix

--- rocq-brick-libstdcpp/proof/mutex/spec/unique_lock.v
+++ rocq-brick-libstdcpp/proof/mutex/spec/unique_lock.v
@@ -69,7 +69,9 @@ Module unique_lock.
       mutex.token g q **
       (* TODO readd *)
       (* ▷ *)
-      (mutex.locked g thr q ** ▷P -* Q)
+      (mutex.locked g thr q ** P -* Q)
+      (* Here, we assume we can return [P] instead of [▷ P] because [lock()] can
+      take a skip step to strip the later before invoking this wand and returning [Q]. *)
     end.
 
   Section with_cpp.
@@ -291,7 +293,7 @@ Module unique_lock.
         \persist{thr} current_thread thr
         \post
           this |-> R 1$m (Some (true, (mp, g, q, P))) **
-          |> P ** mutex.locked g thr q).
+          P ** mutex.locked g thr q).
 
       cpp.spec "std::unique_lock<std::mutex>::lock()" as lock_spec_alt from source with (
         \this this
