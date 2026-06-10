72d582e95a69b0b079bd73b13cab1c6efa83c767
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2026-06-10 18:29:45 +0200

    Templatize unique_lock_recursive_mutex.v

--- rocq-brick-libstdcpp/proof/mutex/spec/unique_lock_recursive_mutex.v
+++ rocq-brick-libstdcpp/proof/mutex/spec/unique_lock_recursive_mutex.v
@@ -1,15 +1,12 @@
 Require Import skylabs.auto.cpp.proof.
 Require Import skylabs.brick.libstdcpp.mutex.inc_hpp.
+Require Import skylabs.brick.libstdcpp.mutex.inc_hpp_templates.
 
 Require Export skylabs.brick.libstdcpp.runtime.pred.
 Require Import skylabs.brick.libstdcpp.mutex.spec.prelude.
-Require Import skylabs.brick.libstdcpp.mutex.spec.recursive_mutex.
 Require Import skylabs.brick.libstdcpp.mutex.requirements.
 
-(* Generic specs for "unique_lock<T>"
-+ instantiation for "std::recursive_mutex"
-TODO: to be replaced with proper template specs.
- *)
+(* Generic specs for "unique_lock<T>" *)
 NES.Begin unique_lock.
   Record M {T : Type} : Type := Mk
   { is_held : bool
@@ -82,43 +79,33 @@ NES.Begin unique_lock.
     Section with_threads.
       Context `{HAS_THREADS : !HasStdThreads Σ}.
 
-      (* abstract over BasicLockable instance. TODO: readd *)
-      (*
-      Context ty {mutexT} mutexR `{!BasicLockable ty (T:=mutexT) mutexR}.
-      *)
+      Context ty {mutexT mutexR} `{!BasicLockable ty (T:=mutexT) mutexR}.
 
-      (* specialize to recursive mutex START. TODO: drop*)
-      Context `{!recursive_mutex.lockedG Σ}.
-      Context `{!HasOwn (iPropI _) recursive_mutex.cmraR}.
-
-      Abbreviation ty := "std::recursive_mutex"%cpp_type (only parsing).
-      Abbreviation mutexT := recursive_mutex.rmutex_gname (only parsing).
-      Abbreviation mutexR' := (λ q γ, recursive_mutex.R γ.(recursive_mutex.lock_gname) q)  (only parsing).
-      Abbreviation mutexR q γ := (recursive_mutex.R γ.(recursive_mutex.lock_gname) q)  (only parsing).
-      (* specialize to recursive mutex END. TODO: drop*)
-
-      #[local] Abbreviation R := (R ty (T:=mutexT) mutexR').
+      #[local] Abbreviation R := (R ty (T:=mutexT) mutexR).
 
       #[global] Instance: LearnEqF1 R := ltac:(solve_learnable).
 
       (* TODO: all the following specs should be generalized over the lock type, except for [lock_spec] and [unlock_spec]. *)
 
-      cpp.spec "std::unique_lock<std::recursive_mutex>::unique_lock()"
-        as default_ctor_spec from source with (
+      cpp.spec "std::unique_lock<$ty>::unique_lock()"
+        as default_ctor_spec from source templates templates (
+        \\with
         \this this
         \post this |-> R 1$m None
       ).
 
-      cpp.spec "std::unique_lock<std::recursive_mutex>::unique_lock(std::recursive_mutex&)" as lock_ctor_spec from source with (
+      cpp.spec "std::unique_lock<$ty>::unique_lock($ty&)" as lock_ctor_spec from source templates templates (
+        \\with
         \this this
         \arg{mp} "" (Vptr mp)
         \pre{q m} mp |-> mutexR q$m m
-        \pre{K} do_lock mp m K
+        \pre{K} do_lock ty mp m K
         \post
           this |-> R 1$m (Some {| is_held := true ; mutex_ptr := mp ; mutex_q := q ; mutex_m := m |}) **
           K).
 
-      cpp.spec "std::unique_lock<std::recursive_mutex>::unique_lock(std::recursive_mutex&, std::defer_lock_t)" as lock_defer_ctor_spec from source with (
+      cpp.spec "std::unique_lock<$ty>::unique_lock($ty&, std::defer_lock_t)" as lock_defer_ctor_spec from source templates templates (
+        \\with
         \this this
         \arg{mp} "" (Vptr mp)
         \pre{q m} mp |-> mutexR q$m m
@@ -126,7 +113,8 @@ NES.Begin unique_lock.
         \post this |-> R 1$m (Some {| is_held := false ; mutex_ptr := mp ; mutex_q := q ; mutex_m := m |})
       ).
 
-      cpp.spec "std::unique_lock<std::recursive_mutex>::unique_lock(std::unique_lock<std::recursive_mutex> &&)" as move_ctor_spec from source with (
+      cpp.spec "std::unique_lock<$ty>::unique_lock(std::unique_lock<$ty> &&)" as move_ctor_spec from source templates templates (
+        \\with
         \this this
         \arg{other} "" (Vptr other)
         \pre{om} other |-> R 1$m om
@@ -142,7 +130,7 @@ NES.Begin unique_lock.
         match om with
         | Some {| is_held := is_held ; mutex_ptr := mp ; mutex_q := q ; mutex_m := m |} =>
           if is_held then
-            letI* := do_unlock mp m in
+            letI* := do_unlock ty mp m in
             (* ▷ *)
             mp |-> mutexR q$m m -* Q
           else
@@ -155,7 +143,8 @@ NES.Begin unique_lock.
 
       #[global] Arguments ensure_unlock /.
 
-      cpp.spec "std::unique_lock<std::recursive_mutex>::~unique_lock()" as dtor_spec from source with (
+      cpp.spec "std::unique_lock<$ty>::~unique_lock()" as dtor_spec from source templates templates (
+        \\with
         \this this
         \pre{om} this |-> R 1$m om
         \pre{K} ensure_unlock om K
@@ -163,13 +152,14 @@ NES.Begin unique_lock.
 
       (** Duplicates [ensure_unlock], but proven equivalent and easier to apply, so
       comes after to be the default. *)
-      cpp.spec "std::unique_lock<std::recursive_mutex>::~unique_lock()" as dtor_spec_alt from source with (
+      cpp.spec "std::unique_lock<$ty>::~unique_lock()" as dtor_spec_alt from source templates templates (
+        \\with
         \this this
         \pre{om} this |-> R 1$m om
         \pre{K}
           match om with
           | Some m =>
-              if m.(is_held) then do_unlock m.(mutex_ptr) m.(mutex_m) K
+              if m.(is_held) then do_unlock ty m.(mutex_ptr) m.(mutex_m) K
               else K
           | _ => K
           end
@@ -179,16 +169,18 @@ NES.Begin unique_lock.
           | None => emp
           end).
 
-      Lemma dtor_spec_alt_entails_dtor_spec : dtor_spec_alt -|- dtor_spec.
+      (*
+      Lemma dtor_spec_alt_entails_dtor_spec : dtor_spec_alt source -|- dtor_spec source.
       Proof.
-        iSplit; iApply specify_mono; work with br_erefl; repeat case_match;
+        iSplit; iApply specify_mono. work with br_erefl; repeat case_match;
           try (exfalso; congruence);
           ework with br_erefl.
         wname [bi_wand] "W".
         iApply ("W" with "[$] [$]").
-      Qed.
+      Qed. *)
 
-      cpp.spec "std::unique_lock<std::recursive_mutex>::operator=(std::unique_lock<std::recursive_mutex> &&)" as move_assign_spec_alt from source with (
+      cpp.spec "std::unique_lock<$ty>::operator=(std::unique_lock<$ty> &&)" as move_assign_spec_alt from source templates templates (
+        \\with
         \this this
         \arg{other} "" (Vptr other)
         \pre{om1} this |-> R 1$m om1
@@ -197,7 +189,7 @@ NES.Begin unique_lock.
         \pre{K}
           match om1 with
           | Some m =>
-              if m.(is_held) then do_unlock m.(mutex_ptr) m.(mutex_m) K
+              if m.(is_held) then do_unlock ty m.(mutex_ptr) m.(mutex_m) K
               else K
           | _ => K
           end
@@ -213,7 +205,8 @@ NES.Begin unique_lock.
 
       (* unlock the associated mutex, if any, and set input as the associated mutex.
       Should be equivalent to move_assign_spec. *)
-      cpp.spec "std::unique_lock<std::recursive_mutex>::operator=(std::unique_lock<std::recursive_mutex> &&)" as move_assign_spec from source with (
+      cpp.spec "std::unique_lock<$ty>::operator=(std::unique_lock<$ty> &&)" as move_assign_spec from source templates templates (
+        \\with
         \this this
         \arg{other} "" (Vptr other)
         \pre{om1} this |-> R 1$m om1
@@ -224,7 +217,7 @@ NES.Begin unique_lock.
           other |-> R 1$m None **
           K
         ).
-
+(*
       Lemma move_assign_spec_alt_entails_move_assign_spec : move_assign_spec_alt -|- move_assign_spec.
       Proof.
         iSplit; iApply specify_mono; work with br_erefl; repeat case_match;
@@ -232,20 +225,25 @@ NES.Begin unique_lock.
           ework with br_erefl.
         wname [bi_wand] "W".
         iApply ("W" with "[$] [$]").
-      Qed.
+      Qed. *)
 
       Abbreviation owns_lock_spec_body := (
         \this this
         \prepost{om q} this |-> R q om
         \post [Vbool (owned om)] emp) (only parsing).
 
-      cpp.spec "std::unique_lock<std::recursive_mutex>::owns_lock() const" as owns_lock_spec
-        from source with (owns_lock_spec_body).
+      cpp.spec "std::unique_lock<$ty>::owns_lock() const" as owns_lock_spec
+        from source templates templates (
+          \\with
+          owns_lock_spec_body).
 
-      cpp.spec "std::unique_lock<std::recursive_mutex>::operator bool() const" as operator_bool_spec
-        from source with (owns_lock_spec_body).
+      cpp.spec "std::unique_lock<$ty>::operator bool() const" as operator_bool_spec
+        from source templates templates (
+          \\with
+          owns_lock_spec_body).
 
-      cpp.spec "std::unique_lock<std::recursive_mutex>::mutex() const" as mutex_spec from source with (
+      cpp.spec "std::unique_lock<$ty>::mutex() const" as mutex_spec from source templates templates (
+        \\with
         \this this
         \prepost{om q} this |-> R q om
         \post[Vptr (mutex om)] emp
@@ -254,20 +252,22 @@ NES.Begin unique_lock.
       (* these preconditions statically rule out cases that throw exceptions, such as:
       - If there is no associated mutex, std::system_error with an error code of std::errc::operation_not_permitted.
       - If the mutex is already locked by this unique_lock (in other words, owns_lock() is true), std::system_error with an error code of std::errc::resource_deadlock_would_occur. *)
-      cpp.spec "std::unique_lock<std::recursive_mutex>::lock()" as lock_spec from source with (
+      cpp.spec "std::unique_lock<$ty>::lock()" as lock_spec from source templates templates (
+        \\with
         \this this
         \pre{mm} this |-> R 1$m (Some mm)
         \require ~~ mm.(is_held)
-        \pre{K} do_lock mm.(mutex_ptr) mm.(mutex_m) K
+        \pre{K} do_lock ty mm.(mutex_ptr) mm.(mutex_m) K
         \post
           this |-> R 1$m (Some (mm &: _is_held .= true)%lens) **
           K).
 
-      cpp.spec "std::unique_lock<std::recursive_mutex>::unlock()" as unlock_spec from source with (
+      cpp.spec "std::unique_lock<$ty>::unlock()" as unlock_spec from source templates templates (
+        \\with
         \this this
         \pre{mm} this |-> R 1$m (Some mm)
         \require mm.(is_held)
-        \pre{K} do_unlock mm.(mutex_ptr) mm.(mutex_m) K
+        \pre{K} do_unlock ty mm.(mutex_ptr) mm.(mutex_m) K
         \post
           this |-> R 1$m (Some (mm &: _is_held .= false)%lens) **
           K
