ac65d11e70a993e11d433a6734c85092830cfbf4
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2026-06-11 17:05:33 +0200

    Restore code proof for unique_lock instances

--- rocq-brick-libstdcpp/proof/mutex/proof/unique_lock_recursive_mutex.v
+++ rocq-brick-libstdcpp/proof/mutex/proof/unique_lock_recursive_mutex.v
@@ -3,10 +3,9 @@ Require Import skylabs.brick.libstdcpp.mutex.inc_hpp.
 
 Require Export skylabs.brick.libstdcpp.runtime.pred.
 Require Import skylabs.brick.libstdcpp.mutex.spec.prelude.
+Require Import skylabs.brick.libstdcpp.mutex.spec.unique_lock.
 Require Import skylabs.brick.libstdcpp.mutex.spec.recursive_mutex.
-Require Import skylabs.brick.libstdcpp.mutex.requirements.
-(*
-Require Import skylabs.brick.libstdcpp.mutex.spec.unique_lock_recursive_mutex.
+Require Import skylabs.brick.libstdcpp.lib.tactics.
 
 NES.Begin unique_lock.
   Section with_cpp.
@@ -18,8 +17,12 @@ NES.Begin unique_lock.
 
     Import R_unfold.
 
-    Lemma default_ctor_spec_ok : verify[source] default_ctor_spec.
-    Proof. verify_spec; go. Qed.
+    Lemma default_ctor_spec_ok :
+      verify[source] "std::unique_lock<std::recursive_mutex>::unique_lock()".
+    Proof.
+      untemplate_goal.
+      verify_spec; go.
+    Qed.
 
     cpp.spec "std::__addressof<std::recursive_mutex>(std::recursive_mutex&)" as __addressof_spec from source with (
       \arg{mp} "" (Vptr mp)
@@ -28,20 +31,36 @@ NES.Begin unique_lock.
 
     #[local] Hint Resolve fractional.UNSAFE_read_prim_learn : sl_opacity.
 
+    Lemma lock_defer_ctor_spec_ok :
+      __addressof_spec |--
+      verify[source]
+        "std::unique_lock<std::recursive_mutex>::unique_lock(std::recursive_mutex&, std::defer_lock_t)".
+    Proof.
+      work; iStopProof; untemplate_goal.
+      (* untemplate_spec (lock_defer_ctor_spec "std::recursive_mutex" source). *)
+
+      verify_spec; go.
+      by rewrite cQp.scale_mut (right_id_L 1%Qp Qp.mul).
+    Qed.
+
     #[local] Instance: `{SplitRecord (M T)} := {}.
 
     Lemma lock_ctor_spec_ok :
     (* needed because [has_dependency] skips builtins *)
       __addressof_spec |--
-      verify[source] lock_ctor_spec.
+      verify[source]
+        "std::unique_lock<std::recursive_mutex>::unique_lock(std::recursive_mutex&)".
     Proof.
+      work; iStopProof; untemplate_goal.
+      (* untemplate_spec (lock_ctor_spec "std::recursive_mutex" source). *)
       verify_spec; go.
-      iExists K, q', q; rewrite cQp.scale_mut right_id_L.
-      go.
+      iExists K.
+      (* Time Succeed solve [setoid_rewrite cQp.scale_mut; setoid_rewrite (right_id_L 1%Qp Qp.mul); ego with br_erefl]. *)
+      (* 1.6s *)
+      iExists _, q.
+      rewrite cQp.scale_mut (right_id_L 1%Qp Qp.mul).
+      go with br_erefl.
     Qed.
 
-    Lemma lock_defer_ctor_spec_ok : __addressof_spec |-- verify[source] lock_defer_ctor_spec.
-    Proof. verify_spec; go. by rewrite cQp.scale_mut right_id_L. Qed.
-
   End with_cpp.
-NES.End unique_lock. *)
+NES.End unique_lock.
