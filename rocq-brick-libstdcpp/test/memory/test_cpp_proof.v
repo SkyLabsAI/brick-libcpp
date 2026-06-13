f92107b69507bd7531244571a5909d8ac15f6501
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2026-06-13 17:08:25 +0200

    Specs of addressof
    Also prove `addressof` correct over `__addressof`.
    Proving `__addressof` correct will need a new axiom for the builtin.

---
+++ rocq-brick-libstdcpp/test/memory/test_cpp_proof.v
@@ -0,0 +1,25 @@
+Require Import skylabs.auto.cpp.prelude.proof.
+
+Require Import skylabs.brick.libstdcpp.memory.spec.addressof.
+Require Import skylabs.brick.libstdcpp.lib.tactics.
+Require Import skylabs.brick.libstdcpp.test.memory.test_cpp.
+
+NES.Begin memory.
+  Section with_cpp.
+    Context `{Σ : cpp_logic, σ : genv}.
+
+    Lemma addressof_ok : __addressof_spec "C" source |-- verify?[source] "std::addressof<C>(C&)".
+    Proof.
+      work; iStopProof; untemplate_goal.
+
+      verify_spec; go.
+    Qed.
+
+    Lemma __addressof_ok : verify?[source] "std::__addressof<C>(C&)".
+    Proof.
+      work; iStopProof; untemplate_goal.
+
+      verify_spec; go.
+    Abort.
+  End with_cpp.
+NES.End memory.
