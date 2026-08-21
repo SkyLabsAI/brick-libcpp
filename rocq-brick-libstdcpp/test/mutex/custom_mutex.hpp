8260d07a844794f06c18d4f01a5136291a3fe7ea
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2026-08-21 11:48:47 +0200

    LLM fixes (up to some remaining races)

--- rocq-brick-libstdcpp/test/mutex/custom_mutex.hpp
+++ rocq-brick-libstdcpp/test/mutex/custom_mutex.hpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <cassert>
 
+#pragma once
 /*
 A mutex, implemented via a spinlock.
 
@@ -15,39 +16,40 @@ std::mutex protocol:
 - can't be destroyed while held.
 */
 class MyMutex {
-  std::atomic<bool> m_lock;
+  std::atomic<bool> m_lock{false};
 
   std::thread::id m_owner{};
 
-  void assert_owner(std::thread::id id) {
-    assert(m_owner == id);
-  }
-  void set_owner(std::thread::id id) {
-    m_owner = id;
+  // "Actual locking"
+  void do_lock() {
+    while (m_lock.exchange(true)) {
+      std::this_thread::yield();
+    }
   }
 
+  void do_unlock() {
+    m_lock = false;
+  }
 public:
 
   void lock() {
     std::thread::id this_id{std::this_thread::get_id()};
+    assert(m_owner != this_id); // benign race. UB?
 
-    assert(m_owner != this_id);
-
-    bool old;
-    while (old = false, !m_lock.compare_exchange_strong(old, true)) {}
+    do_lock(); // start of critical section
 
     m_owner = this_id;
   }
 
   void unlock(){
     assert(m_owner == std::this_thread::get_id());
-
-    m_lock = false;
-
     m_owner = std::thread::id(); // unowned
+
+    do_unlock(); // end of critical section
   }
 
   ~MyMutex() {
+    assert(!m_lock);
     assert(m_owner == std::thread::id()); //unowned
   }
 };
